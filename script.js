01c1d545d7e4bbfcf195361c79bd775b00d12a7b
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-14 16:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	return str
+        .split('')
+        .find(ch => str.indexOf(ch) === str.lastIndexOf(ch)) || null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
